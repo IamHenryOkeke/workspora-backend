f6843609fb62e21511bb0347bb3854455e3d21ed
Okeke Nmesomachukwu Henry <iamhenryokeke@gmail.com>
2026-04-01 11:08:29 +0100

    chore: file rename

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import type { CustomError } from "../lib/type";
+import type { CustomError } from "../lib/types";
 
 export const errorMiddleware = (
   err: CustomError,
