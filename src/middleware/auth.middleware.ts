84853e3dbb8b77667f064aa6126909335f9b724a
Okeke Nmesomachukwu Henry <iamhenryokeke@gmail.com>
2026-04-05 13:07:50 +0100

    feat: added auth middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import passport from "../config/passport-config";
+import { AppError } from "../error/error-handler";
+
+export const isAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  passport.authenticate(
+    "jwt",
+    { session: false },
+    function (err: unknown, user: Express.User, _info: unknown) {
+      if (err) return next(err);
+      if (!user)
+        return next(new AppError("JWT expired. Please login again", 401));
+      req.user = user;
+      return next();
+    },
+  )(req, res, next);
+};
