ecbabddd9d36af92897789e245b4c62c540f1f12
Okeke Nmesomachukwu Henry <iamhenryokeke@gmail.com>
2026-04-07 19:29:08 +0100

    feat: update user and org model

---
+++ prisma/migrations/20260407182650_update_user_and_organization_models/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `logo` to the `Organization` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Organization" ADD COLUMN     "logo" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "deletedAt" TIMESTAMP(3);
